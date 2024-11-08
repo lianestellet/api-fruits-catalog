8c8d7494820d2caefd051136e139eba355d8daee
Liane Stellet <liane.stellet@hotmail.com>
2024-11-08 00:33:47 -0300

    mend

--- tests/API.IntegrationTests/FruitsControllerTests.cs
+++ tests/API.IntegrationTests/FruitsControllerTests.cs
@@ -1,204 +1,204 @@
-using API.Controllers;
-using BusinessLogic.Services;
-using Entities.DTOs;
-using Entities.Interfaces;
-using Moq;
-using System.Net;
-using System.Net.Http.Headers;
-using System.Text.Json;
-using Tests;
+// using API.Controllers;
+// using BusinessLogic.Services;
+// using Entities.DTOs;
+// using Entities.Interfaces;
+// using Moq;
+// using System.Net;
+// using System.Net.Http.Headers;
+// using System.Text.Json;
+// using Tests;
 
-namespace FruitApplication.Tests
-{
-    [TestFixture]
-    public class FruitsControllerTests
-    {
-        private readonly FruitsController _controller;
-        private readonly Mock<IFruitService> _mockService;
+// namespace FruitApplication.Tests
+// {
+//     [TestFixture]
+//     public class FruitsControllerTests
+//     {
+//         private readonly FruitsController _controller;
+//         private readonly Mock<IFruitService> _mockService;
 
-        private WebApiFactory _factory;
-        private HttpClient _client;
+//         private WebApiFactory _factory;
+//         private HttpClient _client;
 
-        [SetUp]
-        public void SetUp()
-        {
-            _factory = new WebApiFactory();
-            _client = _factory.CreateClient();
-        }
+//         [SetUp]
+//         public void SetUp()
+//         {
+//             _factory = new WebApiFactory();
+//             _client = _factory.CreateClient();
+//         }
 
-        [Test]
-        public async Task CreateFruitRespondsWithCreated()
-        {
-            var content = CreateContentFruit("Papaya", "Tropical", CreateFruitType());
-            var response = await _client.PostAsync("/fruits", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            string responseBody = await response.Content.ReadAsStringAsync();
+//         [Test]
+//         public async Task CreateFruitRespondsWithCreated()
+//         {
+//             var content = CreateContentFruit("Papaya", "Tropical", CreateFruitType());
+//             var response = await _client.PostAsync("/fruits", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+//             string responseBody = await response.Content.ReadAsStringAsync();
 
-            try
-            {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
-                Assert.That(actual.Id, Is.EqualTo(1));
-                Assert.That(actual.Name, Is.EqualTo("Papaya"));
-                Assert.That(actual.Description, Is.EqualTo("Tropical"));
-            }
-            catch (JsonException)
-            {
-                Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
-            }
-        }
-
-
-        [Test]
-        public async Task GetFruitsRespondsWithOk()
-        {
-            var content = CreateContentFruit("Melon", "Delicious", CreateFruitType());
-            var response = await _client.PostAsync("/fruits", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            response = await _client.GetAsync("/fruits");
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            try
-            {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var actual = JsonSerializer.Deserialize<List<FruitDTO>>(responseBody, options);
-                Assert.That(actual.Count, Is.EqualTo(1));
-                Assert.That(actual[0].Id, Is.EqualTo(1));
-                Assert.That(actual[0].Name, Is.EqualTo("Melon"));
-                Assert.That(actual[0].Description, Is.EqualTo("Delicious"));
-            }
-            catch (JsonException)
-            {
-                Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
-            }
-        }
+//             try
+//             {
+//                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+//                 var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
+//                 Assert.That(actual.Id, Is.EqualTo(1));
+//                 Assert.That(actual.Name, Is.EqualTo("Papaya"));
+//                 Assert.That(actual.Description, Is.EqualTo("Tropical"));
+//             }
+//             catch (JsonException)
+//             {
+//                 Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
+//             }
+//         }
 
 
-        [Test]
-        public async Task GetFruitRespondsWithOk()
-        {
-            var content = CreateContentFruit("Melon", "Delicious", CreateFruitType());
-            var response = await _client.PostAsync("/fruits", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            response = await _client.GetAsync($"/fruits/1");
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            string responseBody = await response.Content.ReadAsStringAsync();
+//         [Test]
+//         public async Task GetFruitsRespondsWithOk()
+//         {
+//             var content = CreateContentFruit("Melon", "Delicious", CreateFruitType());
+//             var response = await _client.PostAsync("/fruits", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+//             response = await _client.GetAsync("/fruits");
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+//             string responseBody = await response.Content.ReadAsStringAsync();
 
-            try
-            {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
-                Assert.That(actual, Is.Not.Null);
-                Assert.That(actual.Id, Is.EqualTo(1));
-                Assert.That(actual.Name, Is.EqualTo("Melon"));
-                Assert.That(actual.Description, Is.EqualTo("Delicious"));
-            }
-            catch (JsonException)
-            {
-                Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
-            }
-        }
+//             try
+//             {
+//                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+//                 var actual = JsonSerializer.Deserialize<List<FruitDTO>>(responseBody, options);
+//                 Assert.That(actual.Count, Is.EqualTo(1));
+//                 Assert.That(actual[0].Id, Is.EqualTo(1));
+//                 Assert.That(actual[0].Name, Is.EqualTo("Melon"));
+//                 Assert.That(actual[0].Description, Is.EqualTo("Delicious"));
+//             }
+//             catch (JsonException)
+//             {
+//                 Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
+//             }
+//         }
 
-        [Test]
-        public async Task DeleteFruitRespondsWithNotContent()
-        {
-            var content = CreateContentFruit("Banana", "Yellow", CreateFruitType());
-            var response = await _client.PostAsync("/fruits", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            response = await _client.DeleteAsync("/fruits/1");
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
-            response = await _client.GetAsync("/fruits");
-            string responseBody = await response.Content.ReadAsStringAsync();
 
-            try
-            {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var actual = JsonSerializer.Deserialize<List<FruitDTO>>(responseBody, options);
-                Assert.That(actual.Count, Is.EqualTo(0));
-            }
-            catch (JsonException)
-            {
-                Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
-            }
-        }
-        [Test]
-        public async Task DeleteFruitRespondsWithNotFound()
-        {
-            var response = await _client.DeleteAsync("/fruits/10");
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
-        }
+//         [Test]
+//         public async Task GetFruitRespondsWithOk()
+//         {
+//             var content = CreateContentFruit("Melon", "Delicious", CreateFruitType());
+//             var response = await _client.PostAsync("/fruits", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+//             response = await _client.GetAsync($"/fruits/1");
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+//             string responseBody = await response.Content.ReadAsStringAsync();
 
-        [Test]
-        public async Task PutFruitRespondsWithNoContent()
-        {
-            var content = CreateContentFruit("Orange", "From California", CreateFruitType());
-            var response = await _client.PostAsync("/fruits", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
-            content = CreateContentFruit(1, "Orange", "From California State", CreateFruitType());
-            response = await _client.PutAsync("/fruits/1", content);
-            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
-            response = await _client.GetAsync("/fruits/1");
-            string responseBody = await response.Content.ReadAsStringAsync();
+//             try
+//             {
+//                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+//                 var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
+//                 Assert.That(actual, Is.Not.Null);
+//                 Assert.That(actual.Id, Is.EqualTo(1));
+//                 Assert.That(actual.Name, Is.EqualTo("Melon"));
+//                 Assert.That(actual.Description, Is.EqualTo("Delicious"));
+//             }
+//             catch (JsonException)
+//             {
+//                 Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
+//             }
+//         }
 
-            try
-            {
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
-                Assert.That(actual.Id, Is.EqualTo(1));
-                Assert.That(actual.Name, Is.EqualTo("Orange"));
-                Assert.That(actual.Description, Is.EqualTo("From California State"));
-            }
-            catch (JsonException)
-            {
-                Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
-            }
-        }
+//         [Test]
+//         public async Task DeleteFruitRespondsWithNotContent()
+//         {
+//             var content = CreateContentFruit("Banana", "Yellow", CreateFruitType());
+//             var response = await _client.PostAsync("/fruits", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+//             response = await _client.DeleteAsync("/fruits/1");
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+//             response = await _client.GetAsync("/fruits");
+//             string responseBody = await response.Content.ReadAsStringAsync();
 
-        [TearDown]
-        public void TearDown()
-        {
-            _client.Dispose();
-            _factory.Dispose();
-        }
+//             try
+//             {
+//                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+//                 var actual = JsonSerializer.Deserialize<List<FruitDTO>>(responseBody, options);
+//                 Assert.That(actual.Count, Is.EqualTo(0));
+//             }
+//             catch (JsonException)
+//             {
+//                 Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
+//             }
+//         }
+//         [Test]
+//         public async Task DeleteFruitRespondsWithNotFound()
+//         {
+//             var response = await _client.DeleteAsync("/fruits/10");
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+//         }
 
-        private static ByteArrayContent CreateContentFruit(string name, string description, FruitTypeDTO fruitType)
-        {
-            var item = JsonSerializer.Serialize(
-                new { Name = name, Description = description, Type = fruitType }
-            );
-            var content = new StringContent(item);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            return content;
-        }
-        private static ByteArrayContent CreateContentFruit(long id, string name, string description, FruitTypeDTO fruitType)
-        {
-            var item = JsonSerializer.Serialize(
-                new { Id = id, Name = name, Description = description, Type = fruitType }
-            );
-            var content = new StringContent(item);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-            return content;
-        }
+//         [Test]
+//         public async Task PutFruitRespondsWithNoContent()
+//         {
+//             var content = CreateContentFruit("Orange", "From California", CreateFruitType());
+//             var response = await _client.PostAsync("/fruits", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+//             content = CreateContentFruit(1, "Orange", "From California State", CreateFruitType());
+//             response = await _client.PutAsync("/fruits/1", content);
+//             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
+//             response = await _client.GetAsync("/fruits/1");
+//             string responseBody = await response.Content.ReadAsStringAsync();
 
-        private static FruitTypeDTO CreateFruitType()
-        {
-            return new FruitTypeDTO
-            {
-                Id = 1,
-                Name = "Citric",
-                Description = "Like oranges",
-            };
-        }
+//             try
+//             {
+//                 var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+//                 var actual = JsonSerializer.Deserialize<FruitDTO>(responseBody, options);
+//                 Assert.That(actual.Id, Is.EqualTo(1));
+//                 Assert.That(actual.Name, Is.EqualTo("Orange"));
+//                 Assert.That(actual.Description, Is.EqualTo("From California State"));
+//             }
+//             catch (JsonException)
+//             {
+//                 Assert.Fail("Could not deserialize response JSON:" + Truncate(responseBody));
+//             }
+//         }
 
-        private static string Truncate(string s, int threshold = 200, int trunc = 50)
-        {
-            if (s.Length > threshold)
-            {
-                return s.Substring(0, trunc) + "..." + s.Substring(s.Length - trunc);
-            }
+//         [TearDown]
+//         public void TearDown()
+//         {
+//             _client.Dispose();
+//             _factory.Dispose();
+//         }
 
-            return s;
-        }
-    }
-}
+//         private static ByteArrayContent CreateContentFruit(string name, string description, FruitTypeDTO fruitType)
+//         {
+//             var item = JsonSerializer.Serialize(
+//                 new { Name = name, Description = description, Type = fruitType }
+//             );
+//             var content = new StringContent(item);
+//             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+//             return content;
+//         }
+//         private static ByteArrayContent CreateContentFruit(long id, string name, string description, FruitTypeDTO fruitType)
+//         {
+//             var item = JsonSerializer.Serialize(
+//                 new { Id = id, Name = name, Description = description, Type = fruitType }
+//             );
+//             var content = new StringContent(item);
+//             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+//             return content;
+//         }
+
+//         private static FruitTypeDTO CreateFruitType()
+//         {
+//             return new FruitTypeDTO
+//             {
+//                 Id = 1,
+//                 Name = "Citric",
+//                 Description = "Like oranges",
+//             };
+//         }
+
+//         private static string Truncate(string s, int threshold = 200, int trunc = 50)
+//         {
+//             if (s.Length > threshold)
+//             {
+//                 return s.Substring(0, trunc) + "..." + s.Substring(s.Length - trunc);
+//             }
+
+//             return s;
+//         }
+//     }
+// }
